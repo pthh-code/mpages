14df5b9b3dd60be8b87fda3c4a75ec9bec4e9f6b
Hanh Pham <pham@verssa.com>
2020-04-21 18:15:32 -0500

    css3 box

---
+++ css3-box/1.js
@@ -0,0 +1,8 @@
+// chi dinh doi tuong
+var n1 = document.getElementById('nutso1');
+
+var to = document.querySelector('.to');
+
+n1.addEventListener('click',function(){
+	to.classList.toggle('quayX');
+})
